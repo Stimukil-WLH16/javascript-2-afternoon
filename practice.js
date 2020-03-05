ee5a82422ef37eaf74ecdd274e5c9f04cb10c1d3
unknown <jeff_cox76@hotmail.com>
2020-03-05 09:56:47 -0700

    save 01

--- practice.js
+++ practice.js
@@ -15,7 +15,12 @@
 */
 
 //Code here
+let me = {
+  name: 'Jeff',
+  age: 43
+}
 
+me.name
 ////////// PROBLEM 2 //////////
 
 /*
@@ -24,6 +29,14 @@
 */
 
 //Code here
+let favoriteThings = {
+  band: 'Slipknot',
+  food: 'pizza',
+  person: 'Jenalyn',
+  book: 'The Stand',
+  movie: 'A Clockwork Orange',
+  holiday: 'Halloween'
+}
 
 /*
   After you've made your object, use bracket or dot notation to add another key named 'car' with the value being your favorite car
@@ -31,6 +44,8 @@
 */
 
 //Code here
+favoriteThings.car = 'Mustang'
+favoriteThings.brand = 'Vans'
 
 /*
   Now use bracket or dot notation to change the value of the food key in your favoriteThings object to be 'Chicken Nuggets'
@@ -38,6 +53,11 @@
 */
 
 //Code here
+// favoriteThings['food'] = 'Chicken Nuggets'
+// favoriteThings['book'] = 'Harry Potter'
+
+favoriteThings.food = 'Chicken Nuggets'
+favoriteThings.book = 'Harry Potter'
 
 ////////// PROBLEM 3 //////////
 
@@ -51,11 +71,20 @@
 
 //Code here
 
+let backPack = {
+  firstPocket: 'chapstick'
+}
+
+item = 'firstPocket'
+backPack.color = 'black'
+
 /*
   After you do the above, alert your entire backPack object.
 */
 
 //Code here
+
+// alert(backPack)
 
 /*
 You probably noticed that it just alerted [object Object].
@@ -64,6 +93,8 @@ Instead, console.log your whole backPack object and then check out the console.
 */
 
 //Code here
+
+// console.log(backPack)
 
 ////////// PROBLEM 4 //////////
 
@@ -86,6 +117,10 @@ var user2 = {
 
 //Code Here
 
+user2.name = 'Bryan G. Smith'
+user2.email = 'bryan.smith@devmounta.in'
+
+
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
 
@@ -96,6 +131,9 @@ var user2 = {
 */
 
 //Code Here
+let methodCollection = {
+
+}
 
 /*
   Now add two methods (functions that are properties on objects) to your methodCollection object.
@@ -103,6 +141,8 @@ var user2 = {
 */
 
 //Code Here
+
+
 
 /*
   Now call your alertHello and logHello methods.
